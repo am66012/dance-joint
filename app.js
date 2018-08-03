e646c2255c0cf3b494169ea8758a7dcb9d2e6104
abhishek <am66012@gmail.com>
2018-08-03 23:37:32 +0530

    3rd august commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static("views"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.listen(7000, (err) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("your app is running on port: 7000");
+    }
+});
